9a09b33786e8f08cff4bc3cba0a76c0f9e753017
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-20 08:30:36 +0100

    Updates

---
+++ precache-manifest.e2442b67b34ec0e6ba7d75daa06be5ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fed735cb5763017a4746f90243c354a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "dfdda0dcc4edafe5e3ba",
+    "url": "/monsters/static/css/main.6b91a0c4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dfdda0dcc4edafe5e3ba",
+    "url": "/monsters/static/js/main.b362acf9.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
